176531deddeee452ca31872d8f203760f9960c74
Eduardo Sanchez <eddiekidd98@gmail.com>
2023-01-18 23:56:29 -0600

    CRUD functionality added

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -8,7 +8,12 @@ router.post('/create', (req, res) => {
     const contactEntry = contactSchema(req.body);
     contactEntry
         .save()
-        .then((data) => res.json(data))
+        .then((data) => res.json({ 
+            message: {
+                id: data.id,
+                status: 201
+            }
+        }))
         .catch((error) => res.json({message: error}))
 });
 
@@ -27,6 +32,26 @@ router.get('/get-one/:id', (req, res) => {
         .findById(id)
         .then((data) => res.json(data))
         .catch((error) => res.json({message: error}));
+});
+
+//update an user info
+router.put('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+    contactSchema
+        .updateOne({ _id: id }, { $set: {firstName, lastName, email, favoriteColor, birthday}})
+        .then((data) => res.json({ message: {status: 204, data} }))
+        .catch((error) => res.json({message: error}))
+});
+
+//delete a contact
+router.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    contactSchema
+        .remove({_id: id})
+        .then((data) => res.json({message: {
+            status: 200, data
+        }}))
 });
 
 module.exports = router;
